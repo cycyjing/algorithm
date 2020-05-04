7ac2a2e4fed2cb10ad2f82214509c848a5a168bb
cycyjing <cycycyjing@gmail.com>
2020-05-04 17:07:01 +1200

    delete unused files

--- test.js
+++
@@ -1,6 +0,0 @@
-const taxCompute = require('./taxCompute')
-console.log(taxCompute(48000))//8750
-console.log(taxCompute(14000))//1610
-console.log(taxCompute(-1000))//0
-console.log(taxCompute(70000))//15680
-console.log(taxCompute(80000))//19230
